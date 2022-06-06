d99ba6f75d2d678894d3667d46cdbca3aba63994
david_s <davidsurumen@gmail.com>
2022-06-06 23:59:17 +0000

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list == None:
-        print('Empty')
+    if my_list is None:
+        print('[]')
     else:
         for i in my_list:
             print("{}".format(i))
