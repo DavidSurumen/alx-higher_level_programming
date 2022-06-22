b8866ad92f850e9aca253bad022391add03e48f2
david_s <davidsurumen@gmail.com>
2022-06-22 02:01:57 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,8 +45,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] <= 0:
-                for i in range(self.__position[1]):
-                    print()
+            for i in range(self.position[1]):
+                print()
+
             for x in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
