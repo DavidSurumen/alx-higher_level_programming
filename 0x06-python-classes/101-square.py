c19ffb6856011ca40b2c0aea1817fba66aab91f8
david_s <davidsurumen@gmail.com>
2022-06-22 02:32:44 +0000

    advanced task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -85,4 +85,4 @@ class Square:
                 or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value 
+            self.__position = value
