308b80a75d66793d202b3cf1b54f30deba851d3f
david_s <davidsurumen@gmail.com>
2022-07-08 11:24:21 +0000

    corrected failing tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,7 +23,7 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
-    if div < 0:
+    if div == 0:
         raise ZeroDivisionError('division by zero')
 
     if not isinstance(matrix, list):
