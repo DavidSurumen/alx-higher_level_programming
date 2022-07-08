b4f3789678c109ec60fd24588585d2b01a821aa1
david_s <davidsurumen@gmail.com>
2022-07-08 12:56:04 +0000

    corrected failing tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,7 +20,11 @@ def say_my_name(first_name, last_name=""):
     """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
+
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
+
+    if first_name == "":
+        raise ValueError('first_name is required')
 
     print('My name is ' + first_name + ' ' + last_name)
