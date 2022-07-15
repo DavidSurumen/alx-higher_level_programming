22473e2e1c068e9cd3cb7ec2da073d3d2c0eebd9
david_s <davidsurumen@gmail.com>
2022-07-15 00:03:27 +0000

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,9 +5,5 @@ import json
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple
-    data structure (list, dictionary, string, integer,
-    and boolean) for JSON serialization of an object.
-    """
-    obj_js = json.dumps(obj.__dict__)
-    return json.loads(obj_js)
+    """Serializes an object."""
+    return obj.__dict__
