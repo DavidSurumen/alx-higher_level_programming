b4db8ff94f5ff5119a3e644a91ee93a7cba00ec4
David Surumen <davidsurumen@gmail.com>
2022-07-19 04:19:44 +0000

    unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Unittests for the 'Base' class."""
 import unittest
-from modules.base import Base
-from modules.rectangle import Rectangle
-from modules.square import Square
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
