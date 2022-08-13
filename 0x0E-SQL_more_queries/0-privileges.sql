67c5f3b8e95da6d01cd807e36beb477fef75e8da
David Surumen <davidsurumen@gmail.com>
2022-08-13 19:19:03 +0000

    task 18 advanced

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Script that lists all privileges of MySQL users
+-- Show user_0d_1 privileges
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Show user_0d_2 privileges
+SHOW GRANTS FOR user_0d_2@localhost;
+
