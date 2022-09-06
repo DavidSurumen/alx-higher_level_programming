858536c045004df3e8afa2c798b0265b874080f2
David Surumen <davidsurumen@gmail.com>
2022-09-06 16:53:32 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+    let big = 0;
+    let sec = 0;
+
+    for (let i = 2; i < process.argv.length; i++) {
+      arg_ = parseInt(process.argv[i]);
+      if (arg_ > big) {
+        big = arg_;
+      }
+    }
+
+    for (let i = 2; i < process.argv.length; i++) {
+      arg_ = parseInt(process.argv[i]);
+      if (arg_ > sec && arg_ < big) sec = arg_;
+    }
+
+    console.log(sec);
+}
