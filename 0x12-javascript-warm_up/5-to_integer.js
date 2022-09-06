1b7045d6061b5e665df5790396e17ff6556a9a49
David Surumen <davidsurumen@gmail.com>
2022-09-06 15:34:30 +0000

    adhering to semistandard demands

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const num = process.argv[2]
+const num = process.argv[2];
 if (!isNaN(num)) {
-  console.log('My number: ' + parseInt(num))
+  console.log('My number: ' + parseInt(num));
 } else {
-  console.log('Not a number')
+  console.log('Not a number');
 }
