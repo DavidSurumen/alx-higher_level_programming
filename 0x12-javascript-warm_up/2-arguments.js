c127e36cb505d81079d4e74085064183b04d86fb
David Surumen <davidsurumen@gmail.com>
2022-09-06 13:45:58 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ argv.forEach((val, index) => {
   count++;
 });
 if (count < 3) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (count === 3) {
   console.log('Argument found');
 } else {
