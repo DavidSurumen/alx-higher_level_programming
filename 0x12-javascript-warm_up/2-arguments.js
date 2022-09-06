db1a30e8239930e26e330ccfab5e9df14a678866
David Surumen <davidsurumen@gmail.com>
2022-09-06 13:49:56 +0000

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 let count = 0;
+
 argv.forEach((val, index) => {
   count++;
 });
-if (count < 3) {
+
+if (count === 2) {
   console.log('No argument');
 } else if (count === 3) {
   console.log('Argument found');
