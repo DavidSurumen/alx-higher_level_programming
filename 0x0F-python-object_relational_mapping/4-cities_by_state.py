5cc2d22d9fb780a1e9695f30d26b003a83368f26
David Surumen <davidsurumen@gmail.com>
2022-09-16 10:16:42 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all 'cities' from the database 'hbtn_0e_4_usa'"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+
+    cur = conn.cursor()
+    cmd = """SELECT * FROM cities
+            ORDER BY id ASC;"""
+
+    cur.execute(cmd)
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+
+    cur.close()
+    conn.close()
