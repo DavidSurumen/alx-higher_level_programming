bcc91007754e75ef28bcbcd3915ff0630cf5ca17
David Surumen <davidsurumen@gmail.com>
2022-09-26 03:11:40 +0000

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to URL, and display body of response
-curl -sd "email: test@gmail.com&subject: I will always be here for PLD" "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1" -X POST
