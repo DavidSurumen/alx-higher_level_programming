9935b624d23a39c0b557199aeeb96bee630620b6
David Surumen <davidsurumen@gmail.com>
2022-09-30 02:38:32 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(page)))
+    print('\t- content: {}'.format(page))
+    print('\t- utf8 content: {}'.format(page.decode('utf-8')))
