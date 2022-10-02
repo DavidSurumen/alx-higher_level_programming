eb6d8e10b246dd11c22cae1ad4f54cd253edd45d
David Surumen <davidsurumen@gmail.com>
2022-10-02 23:50:15 +0000

    advanced - task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Takes Github credentials and uses Github API to display user
+id."""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    r = requests.get(url)
+    commits = r.json()
+
+    for i in range(10):
+        print("{}: {}".format(commits[i]['sha'], commits[i]['commit']['author']['name']))
