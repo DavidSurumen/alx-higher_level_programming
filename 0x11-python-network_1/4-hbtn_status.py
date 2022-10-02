8094e072c7b4b40fe12765d7148fba5311b4bcf1
David Surumen <davidsurumen@gmail.com>
2022-10-02 00:57:53 +0000

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status using
+requests module."""
+
+if __name__ == '__main__':
+    import requests
+
+    url =  'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    res = r.content.decode('utf-8')
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(res)))
+    print('\t- content: {}'.format(res))
