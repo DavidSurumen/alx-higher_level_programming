9765fd51d14045756ff7869ca2abe4df58af3b8b
David Surumen <davidsurumen@gmail.com>
2022-10-02 01:23:36 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email addr, sends a POST request with
+the email as a parameter, and displays reponse body."""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    
+    # get url and email
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+
+    r = requests.post(url, data=email)
+    print(r.content.decode('utf-8'))
