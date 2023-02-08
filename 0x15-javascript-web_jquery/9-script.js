455add65588b813315ed0bc21769e4b246d31ac2
David Surumen <davidsurumen@gmail.com>
2023-02-08 19:48:16 +0300

    task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,5 @@
-$(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
-    if (textStatus === 'success') {
-      $('DIV#hello').text(data.hello);
-    }
+$(document).ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
   });
 });
